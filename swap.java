bdb78291c6937fa12a580c765b73339ad23a7ed4
Anjali625117 <104921358+Anjali625117@users.noreply.github.com>
2022-05-05 02:11:28 +0530

    updated java file

--- swap.java
+++ swap.java
@@ -1,37 +1,35 @@
-class swap
-{ static int []arr={0,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
-public static void swapp()
-{
-    int low=0,high=14;
-    while(low<high)
-    {
-        while(arr[low]==0)
-       { low++;
-       }
-        while(arr[high]==1)
-    {    high--;
-    }
-        if(low<high)
-        {
-            arr[low]=0;
-            arr[high]=1;
-            low++;high--;
+class Swap {
+    static int[] arr = { 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
+
+    public static void swapp() {
+        int low = 0, high = 14;
+        while (low < high) {
+            while (arr[low] == 0) {
+                low++;
+            }
+            while (arr[high] == 1) {
+                high--;
+            }
+            if (low < high) {
+                arr[low] = 0;
+                arr[high] = 1;
+                low++;
+                high--;
+            }
+
         }
-        
     }
-}
-public static void print()
-{
-    int i=0;
-    while(i<arr.length)
-    {
-        System.out.print(arr[i]+" ");
-        i++;
+
+    public static void print() {
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print(arr[i] + " ");
+            i++;
+        }
     }
-}
-    public static void main(String []args)
-    {
-swapp(); 
-print();
+
+    public static void main(String[] args) {
+        swapp();
+        print();
     }
 }
