6fdb1fa6e7147bd645b3dcc13c262a5c58aff36c
Anjali625117 <104921358+Anjali625117@users.noreply.github.com>
2022-10-16 15:34:34 +0530

    some inap

--- linque.java
+++ linque.java
@@ -1,8 +1,19 @@
-class linque
-{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
+class linque {
     public static void main(String[] args) {
-        System.out.println("bleh bleh bleh");
-        System.out.println("haan haan haan");
-        
+        List<Integer> arr = Arrays.asList(1, 2, 3, 4);
+        ArrayList<Integer> squaredArr = new ArrayList<>();
+
+        // for (int i = 0; i < arr.size(); i++) {
+        //     squaredArr.add(arr.get(i) * arr.get(i));
+        // }
+
+        System.out.println(arr.stream().map(n -> n * n).collect(Collectors.toList()));
+        System.out.println(arr.stream().filter(n -> n % 2 == 0).collect(Collectors.toList()));
+        arr.stream().forEach(n -> System.out.println(n));
     }
 }
---
+++ recur.java
@@ -0,0 +1,10 @@
+class recur
+{
+public static void main(String[] args) {
+     int var=0;
+     if(var==9)
+     System.out.println(var);
+     else
+     System.out.println(var);
+}
+}
